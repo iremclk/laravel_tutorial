0818f085ee112ca13d3f9d9ac8107812d9798610
irem <celk0irem007@gmail.com>
2020-04-09 20:12:17 -1200

    Middleware
    middleware structure examples added

--- L2/routes/web.php
+++ L2/routes/web.php
@@ -21,13 +21,18 @@ Route::post('/form', 'HomeController@post_form');
 
 Route::get('/haberler', 'HomeController@get_haberler');
 Route::post('/haberler', 'HomeController@post_haberler');
-
+/*
 Route::group(['prefix'=>'deneme'], function(){
     Route::get('/php','HomeController@get_deneme');
     Route::get('/python','HomeController@get_deneme');
     Route::get('/go','HomeController@get_deneme');
-});
+});*/
 
+Route::group(['prefix'=>'admin','middleware'=>'admin'], function(){
+    Route::get('/php','HomeController@get_deneme');
+    Route::get('/python','HomeController@get_deneme');
+    Route::get('/go','HomeController@get_deneme');
+});
 
 Route::get('/deneme/{forum}/{php}/{framework}/sorular', 'HomeController@get_kategori');
 Route::get('/deneme/{isim}', 'HomeController@get_url_from_route');//get url from route
